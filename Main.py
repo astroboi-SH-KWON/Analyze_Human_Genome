35473f22a0617ed889b3ef3204f00433af6a0eee
astroboi <terry007x@korea.ac.kr>
2020-04-02 10:29:56 +0900

    진행중

--- Main.py
+++ Main.py
@@ -8,11 +8,19 @@ DIR_PATH = "D:/000_WORK/KimNahye/20200310/"
 CEG_V2 = "CEGv2.txt"
 HGNC_RE = "HGNC_Reference.txt"
 HG38_RE = "hg38_refFlat_full.txt"
+RAW_DATA_DIR = "hg38/Splited/"
+HG38_START = 2 # 배열 중에 필요한 정보가 들어간 배열은 idx = HG38_START 이후에 정보가 있음. 배열 길이가 HG38_START 짧으면 정보 없어서 무시
+MAX_MIS_CNT = 10 # mismatch 최대값
+
+STD_SEQ = "guide_seq.xlsx"
+SHEET_NAME = "Sheet2"
+COL_NAME = "Guide (X20)"
+PAM_SEQ = ['NRG']
 
 
 
-HG38_START = 2
-
+INITIAL_ANALYSIS = [PAM_SEQ, DIR_PATH + RAW_DATA_DIR, MAX_MIS_CNT]
+MAKE_EXCEL = [DIR_PATH, COL_NAME, MAX_MIS_CNT]
 ############### end setting env  ################
 def main():
     util = Utils.Utils()
@@ -26,7 +34,19 @@ def main():
     nmid_dict, null_gene_list = p.add_nmid_by_hgnc_id(ceg_dict, hgnc_list, na_gene_list)
     hg38_full_dict, null_gene_tot_list = p.add_hg38_ref_full(nmid_dict, hg38_list, null_gene_list)
 
-    exon_idx_dict = p.get_exon_dixt(hg38_full_dict, HG38_START)
+    exon_dict_no_filter = p.get_exon_dixt(hg38_full_dict, HG38_START)
+    exon_idx_dict = p.filter_out(exon_dict_no_filter, "_")
+
+    excel_dict = util.get_excel(DIR_PATH + STD_SEQ, SHEET_NAME)
+    tmp_list = util.get_data_by_col(excel_dict, "Guide (X20)")
+    # remove the first guide sequence ( for NaHye's project )
+    std_seq_list = []
+    for tmp_str in tmp_list:
+        std_seq_list.append(tmp_str[1:])
+
+    # total_exon_dict, total_intron_dict = p.get_analysis(exon_idx_dict, std_seq_list, INITIAL_ANALYSIS)
+    total_dict = p.get_analysis2(exon_idx_dict, std_seq_list, INITIAL_ANALYSIS)
+    util.make_excel1(MAKE_EXCEL, total_dict)
 
 
 start_time = clock()
--- Utils.py
+++ Utils.py
@@ -1,8 +1,33 @@
-
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+import openpyxl
+from time import clock
 
 class Utils:
     def __init__(self):
         self.ext_txt = ".txt"
+        self.ext_fa = ".fa"
+        self.ext_xlsx = ".xlsx"
+
+    """
+    get_excel : read excel file by sheet_name then make data to dict
+    :param
+        path :
+        sheet_name :
+    :return
+        dict object : { 'col_name' : {0: 'val0' , 1: 'val1' , 2 : 'val2' ...
+    """
+    def get_excel(self,path,sheet_name):
+        return pd.read_excel(path , sheet_name=sheet_name).to_dict()
+
+    """
+    get_data_by_col : get data by key(=col_name)
+    :return 
+        list object : [ ]
+    """
+    def get_data_by_col(self, tmp_dict,col_name):
+        return tmp_dict[col_name].values()
 
     def read_file_by_line_to_list(self,path):
         tmp_list = []
@@ -13,5 +38,45 @@ class Utils:
                     tmp_list.append(tmp_line)
                 else:
                     break
-
         return tmp_list
+
+    def get_file_list_from_dir(self,path):
+        return [f for f in listdir(path) if isfile(join(path,f))]
+
+    """
+    make_excel : make dictionary data to excel file
+    :param
+         path : 
+         result_dict : 
+    :return
+        excel file in path
+    """
+    def make_excel1(self, param, result_dict):
+        path = param[0]
+        col_name = param[1]
+        max_cnt = param[2]
+
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+
+        row = 1
+        sheet.cell(row=row, column=1, value="INDEX")
+        sheet.cell(row=row, column=2, value=col_name)
+        col_header = 3
+        for i in range(max_cnt + 1):
+            sheet.cell(row=row, column=col_header, value=i)
+            col_header = col_header + 1
+
+        for key, val_cnt in result_dict.items():
+            row = row + 1
+            sheet.cell(row=row, column=1, value=row-1)
+            sheet.cell(row=row, column=2, value=key)
+            col = 3
+            for j in range(max_cnt + 1):
+                if j in val_cnt:
+                    sheet.cell(row=row, column=col, value=len(val_cnt[j]))
+                else:
+                    sheet.cell(row=row, column=col, value='0')
+                col = col + 1
+        workbook.save(filename=path + col_name + "_" + str(clock()) + self.ext_xlsx)
+
